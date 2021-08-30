9f1e95aa66a76540513e7a3b7149e6df168ed6eb
HariBabu Nalluri <harinvb@gmail.com>
2021-08-30 11:11:07 +0530

    updated database preference

--- src/test/java/org/mindtree/customer/CustomerApplicationTests.java
+++ src/test/java/org/mindtree/customer/CustomerApplicationTests.java
@@ -1,5 +1,7 @@
 package org.mindtree.customer;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +9,27 @@ import org.springframework.boot.test.context.SpringBootTest;
 class CustomerApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void Test() {
+		int x=1;
+		assertEquals(1, x);
+	}
+
+	@Test
+	void temporarytest() {
+		int y=1;
+		assertEquals(1, y);
+	}
+
+	@Test
+	void temp2() {
+		int abc=1;
+		assertEquals(1, abc);
+	}
+
+	@Test
+	void temptest3(String s) {
+		int hello=1;
+		assertEquals(1, hello);
 	}
 
 }
